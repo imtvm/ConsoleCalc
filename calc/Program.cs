1abfda9ad7b245b4e8c0970d132ad5d5281b6122
imtvm <t.vergismayo@gmail.com>
2019-01-19 20:52:24 -0500

    Automated Commit

--- calc/Program.cs
+++ calc/Program.cs
@@ -6,7 +6,44 @@ namespace calc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string oper = args[0];
+            double result = 0;
+
+            switch (oper) {
+                case "+":
+                    for (int i = 1; i < args.Length; i++ ) {
+                        result += int.Parse(args[i]);
+                    }
+                    Console.WriteLine(result);
+                    break;
+                case "-":
+                    result = int.Parse(args[1]);
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        result -= int.Parse(args[i]);
+                    }
+                    Console.WriteLine(result);
+                    break;
+                case "/":
+                    result = int.Parse(args[1]);
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        result += int.Parse(args[i]);
+                    }
+                    Console.WriteLine(result);
+                    break;
+                case "*":
+                    result = int.Parse(args[1]);
+                    for (int i = 2; i < args.Length; i++)
+                    {
+                        result *= int.Parse(args[i]);
+                    }
+                    Console.WriteLine(result);
+                    break;
+                default:
+                    Console.WriteLine(oper);
+                    break;
+            }
         }
     }
 }
